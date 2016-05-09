2fb227f425d2a0393aa22eb712a50cd3680bd378
xiaoningwen <daniel.wen@canada.ca>
2016-05-09 11:30:33 -0400

    first commit for file module
    implemented load and populate sidebar at the same time and search for
    updating sidebar

--- app/renderer/scripts/app.js
+++ app/renderer/scripts/app.js
@@ -3,15 +3,14 @@ import ngRouter from 'angular-route';
 import ngMaterial from 'angular-material';
 import ngAnimate from '../jspm_packages/github/angular/bower-angular-animate@1.4.9/angular-animate';
 import ngMessages from 'angular-messages';
-import {LegalEntityService} from './legal_entity/legalEntityService'; 
+import {LegalEntityService} from './legal_entity/legalEntityService';
 import {LegalEntityController} from './legal_entity/legalEntityController';
-import {ReceiverService} from './receiver/receiverService'; 
+import {ReceiverService} from './receiver/receiverService';
 import {ReceiverController} from './receiver/receiverController';
-import {GhstsService} from './ghsts_demo/ghstsService'; 
+import {FileService} from './file/fileService';
+import {FileController} from './file/fileController';
+import {GhstsService} from './ghsts_demo/ghstsService';
 import {GhstsController} from './ghsts_demo/ghstsController';
-import SubmissionController from './submission/submissionController';
-import SubmissionService from './submission/submissionService';
-
 // notice stylesheet loading from app.js
 import '../jspm_packages/github/angular/bower-material@1.0.4/angular-material.css!';
 
@@ -21,39 +20,39 @@ angular.module('ghstsApp', ['ngRoute', 'ngMaterial', 'ngAnimate', 'ngMessages'])
     .controller('legalEntityController', ['$mdDialog', 'legalEntityService', LegalEntityController])
     .service('receiverService', ['$q', ReceiverService])
     .controller('receiverController', ['$mdDialog', 'receiverService', 'legalEntityService', ReceiverController])
-    .service('ghstsService', ['receiverService', 'legalEntityService', 'submissionService', GhstsService])
-    .controller('ghstsController', ['$mdDialog', 'ghstsService', GhstsController])
-    
-    .service('submissionService', ['$q', SubmissionService])
-    .controller('submissionController', ['$mdDialog', 'submissionService']);
+    .service('fileService', ['$q', FileService])
+    .controller('fileController', ['$mdDialog', 'fileService',FileController])
+    .service('ghstsService', ['receiverService', 'legalEntityService', GhstsService])
+    .controller('ghstsController', ['$mdDialog', 'ghstsService', GhstsController]);
 
 function config($routeProvider) {
     $routeProvider
         .when('/home', {
-            templateUrl: './splash.html' ,
+            templateUrl: './splash.html',
             controller: LegalEntityController,
             controllerAs: '_ctrl'
         })
         .when('/demoGHSTS', {
-            templateUrl: './scripts/ghsts_demo/ghsts.html' ,
+            templateUrl: './scripts/ghsts_demo/ghsts.html',
             controller: GhstsController,
             controllerAs: '_ctrl'
         })
         .when('/manageLE', {
-            templateUrl: './scripts/legal_entity/le-manage.html' ,
+            templateUrl: './scripts/legal_entity/le-manage.html',
             controller: LegalEntityController,
             controllerAs: '_ctrl'
         })
         .when('/manageRcvr', {
-            templateUrl: './scripts/receiver/receiver-manage.html' ,
+            templateUrl: './scripts/receiver/receiver-manage.html',
             controller: ReceiverController,
             controllerAs: '_ctrl'
         })
-        .when('/submission', {
-            templateUrl: './scripts/submission/submission-manage.html',
-            controller: SubmissionController,
+        .when('/manageFile', {
+            templateUrl: './scripts/file/file-manager.html',
+            controller: FileController,
             controllerAs: '_ctrl'
-        });
+        }
+        );
     $routeProvider.otherwise({ redirectTo: '/home' });
 }
 
