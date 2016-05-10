88127bfed2b6418134c827e070bf563a30a1d3db
tuhai63 <tuhai63@gmail.com>
2016-05-10 11:20:31 -0400

    Hai's document module
    New files: document-manage.html, documentController.js,
    documentService.js documentModel.js
    Updated files: app.js, ghsts.js, splash.html

--- app/renderer/scripts/app.js
+++ app/renderer/scripts/app.js
@@ -16,6 +16,13 @@ import {DocumentController} from './document/documentController';
 import {GhstsService} from './ghsts_demo/ghstsService'; 
 import {GhstsController} from './ghsts_demo/ghstsController';
 
+import {FileService} from './file/fileService';
+import {FileController} from './file/fileController';
+
+import SubmissionController from './submission/submissionController';
+import SubmissionService from './submission/submissionService';
+
+
 // notice stylesheet loading from app.js
 import '../jspm_packages/github/angular/bower-material@1.0.4/angular-material.css!';
 
@@ -23,45 +30,59 @@ angular.module('ghstsApp', ['ngRoute', 'ngMaterial', 'ngAnimate', 'ngMessages'])
     .config(config)
     .service('legalEntityService', ['$q', LegalEntityService])
     .controller('legalEntityController', ['$mdDialog', 'legalEntityService', LegalEntityController])
-    
     .service('receiverService', ['$q', ReceiverService])
     .controller('receiverController', ['$mdDialog', 'receiverService', 'legalEntityService', ReceiverController])
-    
     .service('documentService', ['$q', DocumentService])
     .controller('documentController', ['$mdDialog', 'documentService', DocumentController])
-    
+    .service('fileService', ['$q', FileService])
+    .controller('fileController', ['$mdDialog', 'fileService',FileController])
     .service('ghstsService', ['receiverService', 'legalEntityService', GhstsService])
-    .controller('ghstsController', ['$mdDialog', 'ghstsService', GhstsController]
-    
-    );
+    .controller('ghstsController', ['$mdDialog', 'ghstsService', GhstsController])
+    .service('submissionService', ['$q', SubmissionService])
+    .controller('submissionController', ['$mdDialog', 'submissionService']);
 
 function config($routeProvider) {
     $routeProvider
         .when('/home', {
-            templateUrl: './splash.html' ,
+            templateUrl: './splash.html',
             controller: LegalEntityController,
             controllerAs: '_ctrl'
         })
         .when('/demoGHSTS', {
-            templateUrl: './scripts/ghsts_demo/ghsts.html' ,
+            templateUrl: './scripts/ghsts_demo/ghsts.html',
             controller: GhstsController,
             controllerAs: '_ctrl'
         })
         .when('/manageLE', {
-            templateUrl: './scripts/legal_entity/le-manage.html' ,
+            templateUrl: './scripts/legal_entity/le-manage.html',
             controller: LegalEntityController,
             controllerAs: '_ctrl'
         })
         .when('/manageRcvr', {
-            templateUrl: './scripts/receiver/receiver-manage.html' ,
+            templateUrl: './scripts/receiver/receiver-manage.html',
             controller: ReceiverController,
             controllerAs: '_ctrl'
         })
+        
+        .when('/submission', {
+            templateUrl: './scripts/submission/submission-manage.html',
+            controller: SubmissionController,
+            controllerAs: '_ctrl'
+        })
+
         .when('/manageDoc', {
             templateUrl: './scripts/document/document-manage.html' ,
             controller: DocumentController,
             controllerAs: '_ctrl'
+        })
+
+        .when('/manageFile', {
+            templateUrl: './scripts/file/file-manager.html',
+            controller: FileController,
+            controllerAs: '_ctrl'
         });
+        
+
     $routeProvider.otherwise({ redirectTo: '/home' });
 }
 
