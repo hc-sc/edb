bc01344c2b1ae21556da0db9f4f2e24871725929
alexgagnon <asgagnon@hotmail.com>
2016-05-25 10:10:49 -0400

    Stashing dossier

--- app/renderer/scripts/ghsts_demo/ghstsService.js
+++ app/renderer/scripts/ghsts_demo/ghstsService.js
@@ -56,64 +56,6 @@ class GhstsService {
                 console.log(`Successfully written to ${outputFile}`);
             })
             .catch(err => console.log(err.stack));
-            
-        // let promise = ghsts.readObjects();
-        // let self = this;
-        
-        // // listen for both fulfillment and rejection        
-        // promise.then(function(contents) {
-        //     console.log('have read from xml into obj.  now about to read from db to get those objects');   
-            
-                     
-
-        //     // add legal entities from database to GHSTS
-        //     let leListPromise = self.legalEntityService.getLegalEntities(); 
-        //     leListPromise.then(function(leList) {
-        //         // console.log('get le lists', leList)                
-        //         leList.forEach(le => { 
-        //             let leObj = new LegalEntity(le);
-        //             ghsts.addLegalEntity(leObj.toGHSTSJson());
-        //         });
-
-        //         // add receivers from database to GHSTS
-        //         let rcvrListPromise = self.receiverService.getReceivers(); 
-        //         rcvrListPromise.then(function(rcvrList) {
-        //             rcvrList.forEach(receiver => {
-        //                 let rcvrObj = new Receiver(receiver);
-        //                 // console.log('receiver json ', rcvrObj.toGHSTSJson())     
-        //                 ghsts.addReceiver(rcvrObj.toGHSTSJson());
-        //             });
-                    
-        //             //replace submission
-        //             //NOTE: at this point, there is no restrictions on how many times we've inserted a submission into the DB, so there may be several rows returned, just grab the first
-        //             let submissionPromise = self.submissionService.getAllSubmissions();
-        //             submissionPromise.then(submission => {
-        //                 const sub = new Submission(submission[0]);
-                        
-        //                 //NOTE: this ghsts has an inner ghsts object that is used to actually write the xml. Default is to append new nodes, rather than replace, so we need to replace it by hand. See console output
-        //                 // console.log(ghsts.ghsts.PRODUCT[0].DOSSIER[0].SUBMISSION);
-        //                 ghsts.addSubmission(sub.toGhstsJson());
-                        
-        //                 // replace Product with Product from the database
-        //                 let productPromise = self.productService.getProducts();
-        //                 productPromise.then(product => {
-        //                     let prodObj = new Product(product[0]);
-                            
-        //                     // TODO: since they are separate for now, this would overwrite setting submission above, so need to reassign
-        //                     ghsts.setProduct(prodObj.toGhstsJson());
-
-                            
-        //                     //we have built the object, output to xml file
-        //                     console.log(ghsts);
-        //                     ghsts.writeXML(outputFile);
-        //                     console.log(`Written to ${outputFile}`);
-        //                 });
-        //             });
-        //         });
-        //     });
-        // }).catch(err => {
-        //     console.log(err);
-        // });  
     }        
 }
 
