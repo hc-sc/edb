6e38a074bafbbc540c54a3347b07965dfa8abba0
xiaoningwen <daniel.wen@canada.ca>
2016-05-25 15:04:58 -0400

    commit merge conflict

--- app/renderer/scripts/app.js
+++ app/renderer/scripts/app.js
@@ -5,32 +5,43 @@ import ngAnimate from '../jspm_packages/github/angular/bower-angular-animate@1.4
 import ngMessages from 'angular-messages';
 import {LegalEntityService} from './legal_entity/legalEntityService';
 import {LegalEntityController} from './legal_entity/legalEntityController';
-import {ReceiverService} from './receiver/receiverService';
+import {ProductService} from './product/productService';
+import {ProductController} from './product/productController';
+import {ReceiverService} from './receiver/receiverService'; 
 import {ReceiverController} from './receiver/receiverController';
-import {GhstsService} from './ghsts_demo/ghstsService';
-import {GhstsController} from './ghsts_demo/ghstsController';
-import SubmissionController from './submission/submissionController';
-import SubmissionService from './submission/submissionService';
+import {SubmissionController} from './submission/submissionController';
+import {SubmissionService} from './submission/submissionService';
 import {FileService} from './file/fileService';
 import {FileController} from './file/fileController';
+import {DocumentService} from './document/documentService'; 
+import {DocumentController} from './document/documentController';
+import {GhstsService} from './ghsts_demo/ghstsService';
+import {GhstsController} from './ghsts_demo/ghstsController';
+import {PickListService} from './common/pickListService';
+
 // notice stylesheet loading from app.js
 import '../jspm_packages/github/angular/bower-material@1.0.4/angular-material.css!';
 
 angular.module('ghstsApp', ['ngRoute', 'ngMaterial', 'ngAnimate', 'ngMessages'])
     .config(config)
+    .service('pickListService', [PickListService])
     .service('legalEntityService', ['$q', LegalEntityService])
-    .controller('legalEntityController', ['$mdDialog', 'legalEntityService', LegalEntityController])
+    .controller('legalEntityController', ['$mdSidenav', '$location', 'pickListService', '$mdDialog', 'legalEntityService', LegalEntityController])
     .service('receiverService', ['$q', ReceiverService])
-    .controller('receiverController', ['$mdDialog', 'receiverService', 'legalEntityService', ReceiverController])
-    .service('ghstsService', ['receiverService', 'legalEntityService', 'submissionService', GhstsService])
+    .controller('receiverController', ['$mdDialog', '$mdSidenav', 'receiverService', 'legalEntityService', ReceiverController])
+    .service('productService', ['$q', ProductService])
+    .controller('productController', ['$mdDialog', 'productService', ProductController])
+    .service('ghstsService', ['receiverService', 'legalEntityService', 'productService', 'submissionService', GhstsService])
     .controller('ghstsController', ['$mdDialog', 'ghstsService', GhstsController])
     .controller('receiverController', ['$mdDialog', 'receiverService', 'legalEntityService', ReceiverController])
     .service('fileService', ['$q', FileService])
     .controller('fileController', ['$mdDialog','$mdSidenav', 'fileService', FileController])
+    .service('documentService', ['$q', DocumentService])
+    .controller('documentController', ['$mdDialog', 'documentService', DocumentController])
     .service('submissionService', ['$q', SubmissionService])
     .controller('submissionController', ['$mdDialog', 'submissionService']);
 
-function config($routeProvider) {
+function config($routeProvider, $mdThemingProvider) {
     $routeProvider
         .when('/home', {
             templateUrl: './splash.html',
@@ -47,6 +58,11 @@ function config($routeProvider) {
             controller: LegalEntityController,
             controllerAs: '_ctrl'
         })
+        .when('/manageProduct', {
+            templateUrl: './scripts/product/product-manage.html',
+            controller: ProductController,
+            controllerAs: '_ctrl'
+        })
         .when('/manageRcvr', {
             templateUrl: './scripts/receiver/receiver-manage.html',
             controller: ReceiverController,
@@ -57,13 +73,23 @@ function config($routeProvider) {
             controller: FileController,
             controllerAs: '_ctrl'
         })
+        .when('/manageDoc', {
+            templateUrl: './scripts/document/document-manage.html' ,
+            controller: DocumentController,
+            controllerAs: '_ctrl'
+        })
             .when('/submission', {
                 templateUrl: './scripts/submission/submission-manage.html',
                 controller: SubmissionController,
                 controllerAs: '_ctrl'
             });
     $routeProvider.otherwise({ redirectTo: '/home' });
+
+    // set the theme
+    $mdThemingProvider.theme('default');
+    // test color
+    //$mdThemingProvider.theme('default').primaryPalette('pink').accentPalette('orange');
 }
 
-config.$inject = ['$routeProvider'];
+config.$inject = ['$routeProvider', '$mdThemingProvider'];
 
