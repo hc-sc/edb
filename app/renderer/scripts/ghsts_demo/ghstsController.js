4a34f44df2ae58b85ba5747b34163a7654f04fc2
hcuser <hcuser>
2016-06-13 12:53:39 -0400

    File Picker is working. Making a fresh commit before doing drastic changes.

--- app/renderer/scripts/ghsts_demo/ghstsController.js
+++ app/renderer/scripts/ghsts_demo/ghstsController.js
@@ -1,15 +1,26 @@
 import angular from 'angular';
 
 class GhstsController {
-    constructor($mdDialog, GhstsService) {
-        this.ghstsService = GhstsService;
-        this.$mdDialog = $mdDialog;     
-    }      
-    
+    constructor($mdDialog, ghstsService) {
+        this.ghstsService = ghstsService;
+        this.$mdDialog = $mdDialog;
+        this.fileName = null;
+
+        // hack to allow the file picker to register that a file has been selected. Angular does not yet support ng-change for input type='file'. Because of this, we need to keep 'this' loose, because we need to access the files value of the DOM element, so can't use fat-arrow. However, because we're calling another service, and we're using class based methods, we need to be able to reference ghstsService that was passed in as a dependency.
+        this.fileInput = document.getElementById('submissionName');
+        this.fileInput.addEventListener(
+            'change',
+            function() {
+                ghstsService.loadXml(this.files[0].name);
+            },
+            false
+        );
+    }
+
     editSubmission() {
         this.ghstsService.loadXml();
     }
-    
+
     assembleGHSTS($event) {
         // call service to assemble Demo GHSTS and report result
         this.ghstsService.assembleDemoGHSTS()
@@ -32,4 +43,3 @@ class GhstsController {
 GhstsController.$inject = ['$mdDialog', 'ghstsService'];
 
 export { GhstsController }
-
--- app/renderer/scripts/ghsts_demo/ghstsService.js
+++ app/renderer/scripts/ghsts_demo/ghstsService.js
@@ -15,9 +15,9 @@ class GhstsService {
         this.legalEntityService = LegalEntityService;
         this.productService = ProductService;
         this.dossierService = DossierService;
-        this.substanceService = SubstanceService; 
+        this.substanceService = SubstanceService;
     }
-    
+
     loadXml() {
         Promise.all([
             this.receiverService.initializeReceivers(),
@@ -29,15 +29,15 @@ class GhstsService {
         .then(() => console.log("Successfully loaded submission"))
         .catch(err => console.log(err.stack));
     }
-            
-    assembleDemoGHSTS(){          
+
+    assembleDemoGHSTS(){
         // this function reads from an existing ghsts.xml file and then
         // overwrites the various nodes in the xml with the collection it finds
         // in the database.  It does not care which one is "correct" from a
         // submission perspective.  For demo purposes only, don't try this
         // at home... I mean in prod.
         let ghsts = new GHSTS("./app/renderer/data/ghsts.xml");
-        
+
         return ghsts.readObjects()
             .then(() => {
                 console.log('XML read into GHSTS object. Read DB to update');
@@ -47,41 +47,40 @@ class GhstsService {
                 for (const le of leList) {
                     ghsts.addLegalEntity(new LegalEntity(le).toGHSTSJson());
                 }
-                
+
                 return this.receiverService.getReceivers();
             })
             .then(rcvrList => {
                 for (const receiver of rcvrList) {
                     ghsts.addReceiver(new Receiver(receiver).toGHSTSJson());
                 }
-                
+
                 return this.productService.getProducts();
             })
             .then(products => {
                 console.log(products);
                 ghsts.setProduct(new Product(products[0]).toGhstsJson());
-                
+
                 return this.dossierService.getDossiers();
             })
             .then(dossiers => {
                 ghsts.setDossier(new Dossier(dossiers[0]).toGhstsJson());
-                
+
                 return this.substanceService.getSubstances();
             })
             .then(substances => {
                 for (const substance of substances) {
-                    ghsts.addSubstance(new Substance(substance).toGHSTSJson());    
+                    ghsts.addSubstance(new Substance(substance).toGHSTSJson());
                 }
-                                
+
                 return ghsts.writeXML(outputFile);
             })
             .then(() => {
                 console.log(`Successfully written to ${outputFile}`);
             });
-    }        
+    }
 }
 
 GhstsService.$inject = [ 'receiverService', 'legalEntityService', 'productService', 'dossierService', 'substanceService'];
 
 export { GhstsService };
-
