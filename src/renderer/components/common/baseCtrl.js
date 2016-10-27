16e9191f3ec382b847d902cdb5f72b943a5d3ce2
xiaoningwen <daniel.wen@canada.ca>
2016-10-27 11:54:12 -0400

    draft base ctrl, substance

---
+++ src/renderer/components/common/baseCtrl.js
@@ -0,0 +1,36 @@
+import { DossierDataService } from '../../services/dossier.data.service';
+//import {GhstsService} from '../../services/ghsts.service';
+export default class BaseCtrl {
+    //url make it specific
+    constructor($state,PicklistService,GhstsService, DossierDataService, url) {//pass specific data service and url
+        let that = this;
+        this.$state=$state;
+        this.url = url;
+        this.pickListService = PicklistService.getService();
+        //  this.GhstsService = GhstsService.getService();
+        this.DossierDataService = DossierDataService.getService();
+        this.records = []; //declare a whole entity instead of some specific fields
+        this.selectedRecord = null;
+        //tool bar
+        this.getRecords({})
+            .then(result => { that.records = result.data; that.selectedRecord = result.data[0]; });
+        /*
+        this.$init = function () {
+            that.getRecords({})
+                .then(result => { that.records = result.data; that.selectedRecord = result.data[0]; });
+        }
+        */
+    }
+    getRecords(data = {}) {
+        return this.DossierDataService.edb_get({ url: this.url, data: {} });
+    }
+    createRecord(data) { // set data with url
+        DossierDataService.edb_put(data);
+    }
+    deleteRecord(data) {
+        DossierDataService.edb_delete(data);
+    }
+    updateRecord(data) {
+        DossierDataService.edb_post();
+    }
+}
