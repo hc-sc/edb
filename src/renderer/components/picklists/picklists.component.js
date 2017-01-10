666a691dfdc2424ed94a6571e6f69c7460d7ee2e
alexgagnon <asgagnon@hotmail.com>
2017-01-10 09:34:58 -0500

    Removed picklist specific code from accord Updated picklists

--- src/renderer/components/picklists/picklists.component.js
+++ src/renderer/components/picklists/picklists.component.js
@@ -5,6 +5,7 @@ import ngMaterial from 'angular-material';
 import Accordion from '../common/accordion/accordion.component';
 
 import template from './picklists.template';
+import './picklists.scss';
 
 export default angular.module('picklists', [
   uiRouter,
@@ -17,7 +18,34 @@ export default angular.module('picklists', [
     picklists: '<'
   },
   controller: class PicklistCtrl {
-    constructor() {}
+    constructor() {
+      this.expandIcon = {name: 'right', label: 'Expand'};
+      this.collapseIcon = {name: 'down', label: 'Collapse'};
+      this.items = this.picklists;
+      this.promoteItems();
+    }
+
+    promoteItems() {
+      this.items = this.items.map(item => {
+        item.items.map(subitem => {
+          return subitem.enabled = subitem.status === 'enabled' ? true : false
+        });
+
+        return Object.assign(
+          item,
+          {title: item.type, showing: false}
+        );
+      });
+    }
+
+    toggleShow(item) {
+      this.items.forEach(i => {
+        if (i.title === item.title) {
+          i.showing = item.showing ? false : true;
+        }
+        else i.showing = false;
+      });
+    }
   }
 })
 .name;
