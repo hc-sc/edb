9d44e1d6cd05801b10d579c1269d2ca4ea947d2c
alexgagnon <asgagnon@hotmail.com>
2017-01-12 16:38:23 -0500

    Sanitize Tree

--- src/renderer/config/app.translations.js
+++ src/renderer/config/app.translations.js
@@ -1,7 +1,5 @@
 import {picklists_en, propNames_en} from '../../configs/translations';
 
-console.log(picklists_en);
-
 export default function($translateProvider) {
   $translateProvider.useSanitizeValueStrategy('escapeParameters');
   $translateProvider.translations('en', Object.assign({},
