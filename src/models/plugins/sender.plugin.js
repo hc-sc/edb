1cd3371219e25819f9114e178b1df11a814cf4f6
xiang.tang <xiang.tang@canada.ca>
2017-02-20 13:11:14 -0500

    Bump-up to 1.9

--- src/models/plugins/sender.plugin.js
+++ src/models/plugins/sender.plugin.js
@@ -2,8 +2,9 @@ module.exports = exports = function SenderPlugin(schema, options) {
   schema.remove('toLegalEntityId');
   schema.add({
     toLegalEntityId: {type: 'ObjectId', ref: 'LEGALENTITY'},
+    _shortname: {type: String}
   });
   schema.virtual('valuedecode').get(function () {
-    return this.remark;
+    return this._shortname;
   });
 };
