23cee276a914451d8910cf62b1792891119f70b2
xiang.tang <xiang.tang@canada.ca>
2017-03-07 16:49:14 -0500

    bug fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { dialog } = require('electron');
 process.on('uncaughtException', (err) => {
   ghstsLogger.error(err);
   dialog.showErrorBox('Uncaught Exception', 'Uncaught exception, application will close. Please check log file');
-  process.exit(1);
+  process.nextTick(() => {process.exit(1);});
 });
 
 process.on('unhandledRejection', (err, p) => {
@@ -138,27 +138,27 @@ var initDB = () => {
         let svrClass;
         let svr;
 
-        // svrClass = require('./services/product.service');
-        // svr = new svrClass('01.00.02');
-        // qAry.push(svr.initDbfromTestData());
+        svrClass = require('./services/product.service');
+        svr = new svrClass('01.00.02');
+        qAry.push(svr.initDbfromTestData());
         svrClass = require('./services/legalentity.service');
         svr = new svrClass('01.00.02');
         qAry.push(svr.initDbfromTestData());
         svrClass = require('./services/substance.service');
         svr = new svrClass('01.00.02');
         qAry.push(svr.initDbfromTestData());
-        // svrClass = require('./services/file.service');
-        // svr = new svrClass('01.00.02');
-        // qAry.push(svr.initDbfromTestData());
-        // svrClass = require('./services/document.service');
-        // svr = new svrClass('01.00.02');
-        // qAry.push(svr.initDbfromTestData());
-        // svrClass = require('./services/receiver.service');
-        // svr = new svrClass('01.00.02');
-        // qAry.push(svr.initDbfromTestData());
-        // svrClass = require('./services/sender.service');
-        // svr = new svrClass('01.00.02');
-        // qAry.push(svr.initDbfromTestData());
+        svrClass = require('./services/file.service');
+        svr = new svrClass('01.00.02');
+        qAry.push(svr.initDbfromTestData());
+        svrClass = require('./services/document.service');
+        svr = new svrClass('01.00.02');
+        qAry.push(svr.initDbfromTestData());
+        svrClass = require('./services/receiver.service');
+        svr = new svrClass('01.00.02');
+        qAry.push(svr.initDbfromTestData());
+        svrClass = require('./services/sender.service');
+        svr = new svrClass('01.00.02');
+        qAry.push(svr.initDbfromTestData());
         svrClass = require('./services/toc.service');
         svr = new svrClass('01.00.02');
         qAry.push(svr.initDbfromTestData());
@@ -328,7 +328,7 @@ app.on('ready', function () {
   mainWindow.loadURL('file://' + __dirname + '/renderer/index.html');
   mainWindow.webContents.on('did-finish-load', function () {
     // TODO: setTitle is being deprecated, find and use alternative
-    mainWindow.setTitle("eDossier Builder (V1.11.0 DRAFT)");
+    mainWindow.setTitle('eDossier Builder (V1.11.0 DRAFT)');
     //if (configure.env.toString().toUpper() == 'DEV'){
     mainWindow.openDevTools();
   });
--- src/models/plugins/submission.plugin.js
+++ src/models/plugins/submission.plugin.js
@@ -8,10 +8,7 @@ module.exports = exports = function SubmissionPlugin(schema, options) {
   schema.add({
     _ghsts: {type: 'ObjectId', ref: 'GHSTS'}
   });
-  schema.post('find', ret => {
-    let retVal = ret.map(item => {
-      item._doc.valuedecode = item._doc.submissiontitle;
-    });
-    return retVal;
+  schema.virtual('valuedecode').get(function () {
+    return this.submissiontitle;
   });
 };
