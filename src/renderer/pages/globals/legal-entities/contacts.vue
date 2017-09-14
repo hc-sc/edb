ba9970157f74326e9b87ad58285083d26dbc888e
Alex Gagnon <asgagnon@hotmail.com>
2017-09-14 16:46:52 -0400

    Fixed 'organisation' typo

--- src/renderer/pages/globals/legal-entities/contacts.vue
+++ src/renderer/pages/globals/legal-entities/contacts.vue
@@ -7,7 +7,7 @@
     <vue-input id='email' :label='$t("email")' v-model='model.email' :max='255'></vue-input>
     <vue-input id='mobile' :label='$t("mobile")' v-model='model.mobile' :max='255'></vue-input>
     <vue-input id='fax' :label='$t("fax")' v-model='model.fax' :max='255'></vue-input>
-    <vue-input id='organization' :label='$t("organization")' v-model='model.organization' :max='255'></vue-input>
+    <vue-input id='organisation' :label='$t("ORGANISATION")' v-model='model.organisation' :max='255'></vue-input>
     <vue-input id='department' :label='$t("department")' v-model='model.department' :max='255'></vue-input>
   </div>
 </template>
@@ -35,7 +35,7 @@ export default {
         email: '',
         mobile: '',
         fax: '',
-        organization: '',
+        organisation: '',
         department: ''
       };
     }
