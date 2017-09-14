1171be7d0be7c92695682561a1f79ff3204a007f
Alex Gagnon <asgagnon@hotmail.com>
2017-09-14 16:40:50 -0400

    Removed files

--- src/renderer/services/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-let services = angular.module('app.services', []);
-
-import ModelService from './model.service';
-services.service('ModelService', ModelService);
-
-export default services.name;
--- src/renderer/services/modal.service.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-
-export default angular.module('ModalService', [])
-.service('ModalService', class ModalService {
-
-})
-.name;
