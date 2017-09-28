051ef508a76ff29d617578257eeafc76d4389171
Alex Gagnon <asgagnon@hotmail.com>
2017-09-28 14:50:37 -0400

    removed vue-toasted

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,7 +3,6 @@ import Vue from 'vue';
 import AsyncComputed from 'vue-async-computed';
 import Dialog from '@/plugins/dialog/dialog.js';
 import Snackbar from '@/plugins/snackbar/snackbar.js';
-import Toast from 'vue-toasted';
 import {router} from './router/';
 import {store} from './store/';
 import {i18n} from './i18n/';
@@ -13,7 +12,6 @@ import '@/assets/css/main.css';
 Vue.config.productionTip = false;
 Vue.use(Dialog);
 Vue.use(Snackbar);
-Vue.use(Toast);
 Vue.use(AsyncComputed);
 
 new Vue({
