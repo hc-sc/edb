77786cf9bdda91d4e2dfbf21a80645643f9298b5
alexgagnon <asgagnon@hotmail.com>
2017-11-15 15:22:40 -0500

    Select working

--- src/renderer/components/select/select.vue
+++ src/renderer/components/select/select.vue
@@ -42,14 +42,54 @@ TODO: clean up focusable
 </docs>
 
 <template>
-  <div class='select' :id='id' :class='{disabled, required}' @focusout='touched = true'>
-    <span :id='`${id}-label`' hidden>{{label}}</span>
-    <input ref='search' type='search' role='search' hidden novalidate>
-    <span aria-hidden>▼</span>
-    <button ref='button' type='button' aria-haspopup='true' :aria-expanded='expanded' @click='toggle'>{{buttonLabel}}</button>
-    <ul ref='listbox' role='listbox' :aria-labelledby='`${id}-label`' :aria-multiselectable='multiple' :aria-activedescendant='activeDescendant'>
-      <li v-for='(option, index) of options' :key='index' :id='`${id}-option-${index}`' role='option' :aria-selected='isSelected(option)' :disabled='isDisabled(option)' @keydown.stop.prevent='handleKeyPress'>{{getOptionLabel(option)}}</li>
-    </ul>
+  <div
+    class='select'
+    :id='id'
+    :class='{disabled, required}'
+    @focusout='touched = true'>
+      <span
+        :id='`${id}-label`'
+        hidden>
+          {{label}}
+      </span>
+      <input
+        ref='search'
+        type='search'
+        role='search'
+        hidden
+        novalidate>
+      <span aria-hidden>▼</span>
+      <button
+        ref='button'
+        type='button'
+        aria-haspopup='true'
+        :aria-expanded='expanded'
+        @click='toggle'>
+          {{buttonLabel}}
+      </button>
+      <div
+        ref='overlay'
+        class='overlay'
+        @click.stop='close'>
+      </div>
+      <ul
+        ref='listbox'
+        role='listbox'
+        :aria-labelledby='`${id}-label`'
+        :aria-multiselectable='multiple' :aria-activedescendant='activeDescendant'>
+          <li
+            v-for='(option, index) of options'
+            :key='index'
+            :id='`${id}-option-${index}`'
+            role='option'
+            :aria-selected='isSelected(index)'
+            :disabled='disabled || isDisabled(option)'
+            tabindex='-1'
+            @keydown.stop.prevent='handleKeyPress($event, index)'
+            @click.stop='handleClick($event, index)'>
+              {{getOptionLabel(option)}}
+          </li>
+      </ul>
   </div>
 </template>
 
@@ -97,6 +137,12 @@ TODO: clean up focusable
       disabled: {
         type: Boolean
       },
+      isDisabled: {
+        type: Function,
+        default(option) {
+          return option.disabled;
+        }
+      },
       required: {
         type: Boolean
       }
@@ -106,101 +152,180 @@ TODO: clean up focusable
         touched: false,
         dirty: false,
         expanded: false,
-        selected: null,
-        focused: null,
-        focusableOptions: [],
+        focusedIndex: -1,
+        selectedIndexes: [],
         searchTerm: '',
-        lastX: undefined,
-        lastY: undefined,
-        firstTransform: null,
-        scrollable: []
+        offsets: {}
       };
     },
     computed: {
+      invalid() {
+        return false;
+      },
       activeDescendant() {
-        return this.focused == null ? false : this.focused.id;
+        return this.focusedIndex < 0 ? false : this.options[this.focusedIndex].id;
       },
       buttonLabel() {
-        return this.label;
+        if (this.selectedIndexes == null || this.selectedIndexes.length === 0) return this.label;
+        return this.selectedIndexes.map(index => {
+          return this.getOptionLabel(this.options[index]);
+        }).join(',');
       }
     },
     methods: {
+      isSelected(index) {
+        return this.selectedIndexes.includes(index);
+      },
+
       toggle() {
         this.expanded ? this.close() : this.open();
       },
 
       open() {
-        this.lastX = window.scrollX;
-        this.lastY = window.scrollY;
-        this.findContainers();
-        this.setListboxPosition();
+        this.selectedIndexes.sort();
+        this.offsets = this.getContainerTranslationOffsets();
+        this.setOverlayProperties();
+        this.setListboxProperties();
         this.expanded = true;
-
-        window.addEventListener('resize', this.setListboxPosition);
-        document.addEventListener('click', () => {
-          if (!this.$refs.listbox.contains(event.target) && !this.$refs.button.contains(event.target)) {
-            this.close();
-          }
-        }, {
-          once: true,
-          capture: true
+        this.$nextTick(() => {
+          let firstSelected = this.getFirstSelectedNode();
+          if (firstSelected) firstSelected.focus();
         });
+
+        window.addEventListener('resize', this.setListboxProperties);
       },
 
       close() {
+        window.removeEventListener('resize', this.setListboxProperties);
         this.expanded = false;
-      },
-
-      isFocused() {},
-
-      isSelected() {},
-
-      isDisabled(option) {
-        return option.disabled;
+        this.focused = -1;
       },
 
       getOptionLabel(option) {
-        this.displayValue(option);
+        return this.displayValue(option);
       },
 
-      handleScroll() {
-        console.log('here');
-        window.scrollTo(0, this.lastY);
-      },
-
-      setListboxPosition() {
+      setListboxProperties() {
         let dims = this.$refs.button.getBoundingClientRect();
-        let containerDims = this.firstTransform.getBoundingClientRect();
-        let offsetX = (dims.x || dims.left) - (containerDims.x || containerDims.left);
-        let offsetY = (dims.y || dims.top) - (containerDims.y || containerDims.top);
+        let offsetX = (dims.x || dims.left) - (this.offsets.x);
+        let offsetY = (dims.y || dims.top) - (this.offsets.y);
 
         this.$refs.listbox.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+        this.$refs.listbox.style.width = `${dims.width}px`;
+      },
+
+      setOverlayProperties() {
+        this.$refs.overlay.style.transform = `translate(-${this.offsets.x}px, -${this.offsets.y}px)`;
+      },
+
+      getContainerTranslationOffsets() {
+        const dims = this.findContainers().firstTransform.getBoundingClientRect();
+        return {
+          x: dims.x || dims.left,
+          y: dims.y || dims.top
+        };
       },
 
       findContainers() {
         let node = this.$el;
+        const result = {
+          firstTransform: document.body,
+        };
         while (node != document.body) {
-          let style = window.getComputedStyle(node);
+          const style = window.getComputedStyle(node);
           if (
-            this.firstTransform == null &&
-            (
-              style.transform != 'none' ||
-              style.perspective != 'none'
-            )
+            style.transform != 'none' ||
+            style.perspective != 'none'
           ) {
-            this.firstTransform = node;
-          }
-          if (node.scrollHeight > parseFloat(style.height)) {
-            this.scrollable.push(node);
+            result.firstTransform = node;
+            break;
           }
           node = node.parentElement;
         }
-        if (this.firstTransform == null) this.firstTransform = document.body;
+        return result;
+      },
+
+      handleClick(event, index) {
+        if (this.isDisabled(this.options[index])) return;
+        this.focusIndex(index);
+        if (!this.multiple) {
+          this.selectIndex(index);
+          this.close();
+          this.$refs.button.focus();
+        }
+        else {
+          this.toggleIndex(index);
+        }
+      },
+
+      handleKeyPress(event, index) {
+        console.log(index);
+      },
+
+      toggleIndex(option) {
+        // emit the event to change
+        let index = this.selectedIndexes.indexOf(option);
+        if (index >= 0) this.selectedIndexes.splice(index, 1);
+        else this.selected.push(option);
+      },
+
+      selectIndex(index) {
+        // emit the event to change, NOTE right now expects single string
+        if (!this.multiple) {
+          this.onSelect(this.options[index]);
+        }
+        else {
+          this.toggleIndex(index);
+        }
+      },
+
+      focusIndex(index) {
+        let option = this.options[index];
+        if (this.isDisabled(option)) return;
+        this.focusedIndex = index;
+        this.$refs.listbox.children[index].focus();
+      },
+
+      focusNext(node, index) {
+        let curr = node;
+        while (curr.hasAttribute('disabled')) {
+          curr = curr.nextElementSibling;
+          ++index;
+          if (index >= this.options.length) return;
+        }
+        this.focusIndex(index);
+      },
+
+      focusPrevious(node, index) {
+        let curr = node;
+        while (curr.hasAttribute('disabled')) {
+          curr = curr.nextElementSibling;
+          --index;
+          if (index < 0) return;
+        }
+        this.focusIndex(index);
+      },
+
+      getFirstSelectedNode() {
+        return this.$refs.listbox.querySelector('[aria-selected=true]');
+      },
+
+      mapValuesToSelected() {
+        let match = this.matchValue(this.options, this.value);
+        console.log(match);
+        if (match >= 0) this.selectedIndexes = [match];
       }
     },
     watch: {
-      value() {},
-      options() {}
+      value() {
+        this.mapValuesToSelected();
+      },
+      options() {
+        this.mapValuesToSelected();
+      }
+    },
+    created() {
+      this.mapValuesToSelected();
     }
   };
 </script>
@@ -213,6 +338,21 @@ TODO: clean up focusable
 .select {
   position: relative;
   width: 100%;
+}
+
+.select > button + .overlay {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+  z-index: 9999;
+}
+
+.select > button[aria-expanded=true] + .overlay {
+  display: block;
 }
 
 .select > button {
@@ -233,33 +373,33 @@ TODO: clean up focusable
   z-index: -1;
 }
 
-.select > button + [role=listbox] {
+.select > button ~ [role=listbox] {
   display: block;
   background-color: white;
   position: fixed;
   top: 0;
   left: 0;
   min-width: 250px;
-  z-index: 9998;
+  z-index: 10000;
   list-style: none;
   box-shadow: 1px 2px;
   padding: 0;
   max-height: 400px;
-  /* overflow: hidden;;
+  overflow: hidden;;
   visibility: hidden;
   opacity: 0;
   transform: scaleY(0);
   transform-origin: top;
-  transition: .2s linear; */
+  /* transition: .2s linear; */
   display: none;
 }
 
-.select > button[aria-expanded=true] + [role=listbox] {
-  /* visibility: visible;
+.select > button[aria-expanded=true] ~ [role=listbox] {
+  visibility: visible;
   overflow-y: auto;
   opacity: 1;
   transform: scaleY(1);
-  transition: opacity .2s .1s linear, transform .4s; */
+  /* transition: opacity .2s .1s linear, transform .4s; */
   display: block;
 }
 
